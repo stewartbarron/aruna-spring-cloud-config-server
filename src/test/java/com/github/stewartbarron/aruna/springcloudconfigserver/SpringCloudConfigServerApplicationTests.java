c34dd0b5ee700a9966a83478be0413071f78f83d
Stewart Barron <stewartbarron@Stewarts-iMac.local>
2021-12-12 23:34:40 +0000

    intial commit

---
+++ src/test/java/com/github/stewartbarron/aruna/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stewartbarron.aruna.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
